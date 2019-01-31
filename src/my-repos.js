446da70c2852cb31e6e41baed3f0863fe6681b6a
Chris Pfaff <chrispfaff10@gmail.com>
2019-01-30 21:16:50 -0600

    updaded myrepos

--- src/my-repos.js
+++ src/my-repos.js
@@ -3,9 +3,9 @@ import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
 
 const reposQuery = gql`
-  {
+  query Myrepositories($first: Int!) {
     viewer {
-      repositories(first: 10) {
+      repositories(first: $first) {
         edges {
           node {
             name
@@ -17,19 +17,37 @@ const reposQuery = gql`
 `;
 
 class Myrepos extends Component {
+  handleMore = (data, fetchMore, current) => {
+    fetchMore({
+      variables: { first: current + 10 },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) {
+          return prev;
+        }
+        return Object.assign(prev, fetchMoreResult);
+      }
+    });
+  };
+
   render() {
     return (
-      <Query query={reposQuery}>
-        {({ data, loading, error }) => {
+      <Query query={reposQuery} variables={{ first: 10 }}>
+        {({ data, loading, error, fetchMore }) => {
           if (loading) return <p>Loading</p>;
           if (error) return <p>{error.message}</p>;
+          let current = data.viewer.repositories.edges.length;
           return (
-            <ul>
-              <h2>First Ten Repos</h2>
-              {data.viewer.repositories.edges.map(({ node }) => (
-                <li key={node.name}>{node.name}</li>
-              ))}
-            </ul>
+            <div>
+              <ul>
+                <h2>First {current} Repos</h2>
+                {data.viewer.repositories.edges.map(({ node }) => (
+                  <li key={node.name}>{node.name}</li>
+                ))}
+              </ul>
+              <button onClick={() => this.handleMore(data, fetchMore, current)}>
+                load more
+              </button>
+            </div>
           );
         }}
       </Query>
